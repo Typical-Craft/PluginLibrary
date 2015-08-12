61768d60da8d1830e8f666304427e961afe5e4a5
Staartvin <mijnleraar@msn.com>
2015-08-12 18:22:36 +0200

    Add listeners to update custom stats.
    Signed-off-by: Staartvin <mijnleraar@msn.com>

---
+++ src/me/staartvin/plugins/pluginlibrary/listeners/PlayerEatsFoodListener.java
@@ -0,0 +1,61 @@
+package me.staartvin.plugins.pluginlibrary.listeners;
+
+import me.staartvin.plugins.pluginlibrary.Library;
+import me.staartvin.plugins.pluginlibrary.PluginLibrary;
+import me.staartvin.plugins.pluginlibrary.hooks.StatsHook;
+import me.staartvin.plugins.pluginlibrary.hooks.customstats.FoodEatenStat;
+import me.staartvin.plugins.pluginlibrary.util.Util;
+import nl.lolmewn.stats.api.stat.Stat;
+import nl.lolmewn.stats.api.user.StatsHolder;
+import nl.lolmewn.stats.stat.DefaultStatEntry;
+import nl.lolmewn.stats.stat.MetadataPair;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerItemConsumeEvent;
+
+/**
+ * This listener will listen to players eating food (for custom stat)
+ * 
+ * @author Staartvin
+ * 
+ */
+public class PlayerEatsFoodListener implements Listener {
+
+	private final PluginLibrary plugin;
+	private final StatsHook hook;
+
+	public PlayerEatsFoodListener(final PluginLibrary instance) {
+		plugin = instance;
+		hook = (StatsHook) PluginLibrary.getLibrary(Library.STATS);
+	}
+
+	@EventHandler
+	public void OnEat(final PlayerItemConsumeEvent event) {
+
+		if (event.isCancelled())
+			return;
+
+		// Stats is not available
+		if (!plugin.isLibraryLoaded(Library.STATS))
+			return;
+
+		Player p = event.getPlayer();
+
+		String foodName = Util.getFoodName(event.getItem());
+
+		if (foodName == null)
+			return;
+
+		Stat stat = hook.getStat(FoodEatenStat.statName);
+
+		StatsHolder holder = hook.getStatsHolder(p.getUniqueId());
+
+		holder.addEntry(stat, new DefaultStatEntry(1, new MetadataPair(
+				"foodType", foodName), new MetadataPair("world", p.getWorld()
+				.getName())));
+
+	}
+
+}
---
+++ src/me/staartvin/plugins/pluginlibrary/listeners/PlayerKillsMobListener.java
@@ -0,0 +1,113 @@
+package me.staartvin.plugins.pluginlibrary.listeners;
+
+import me.staartvin.plugins.pluginlibrary.Library;
+import me.staartvin.plugins.pluginlibrary.PluginLibrary;
+import me.staartvin.plugins.pluginlibrary.hooks.StatsHook;
+import me.staartvin.plugins.pluginlibrary.hooks.customstats.MobKilledStat;
+import nl.lolmewn.stats.api.stat.Stat;
+import nl.lolmewn.stats.api.user.StatsHolder;
+import nl.lolmewn.stats.stat.DefaultStatEntry;
+import nl.lolmewn.stats.stat.MetadataPair;
+
+import org.bukkit.entity.Chicken;
+import org.bukkit.entity.Creeper;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Guardian;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Rabbit;
+import org.bukkit.entity.Rabbit.Type;
+import org.bukkit.entity.Skeleton;
+import org.bukkit.entity.Skeleton.SkeletonType;
+import org.bukkit.entity.Spider;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDeathEvent;
+
+/**
+ * This listener will listen to players killing mobs (for custom stat)
+ * 
+ * @author Staartvin
+ * 
+ */
+public class PlayerKillsMobListener implements Listener {
+
+	private final PluginLibrary plugin;
+	private final StatsHook hook;
+
+	public PlayerKillsMobListener(final PluginLibrary instance) {
+		plugin = instance;
+		hook = (StatsHook) PluginLibrary.getLibrary(Library.STATS);
+	}
+
+	@EventHandler
+	public void OnKill(final EntityDeathEvent event) {
+
+		// Stats is not available
+		if (!plugin.isLibraryLoaded(Library.STATS))
+			return;
+
+		final Entity e = event.getEntity();
+		if (e.getLastDamageCause() instanceof EntityDamageByEntityEvent) {
+			final EntityDamageByEntityEvent nEvent = (EntityDamageByEntityEvent) e
+					.getLastDamageCause();
+			if (nEvent.getDamager() instanceof Player) {
+
+				String extraType = null;
+
+				if (e instanceof Skeleton) {
+					Skeleton ske = (Skeleton) e;
+
+					if (ske.getSkeletonType() == SkeletonType.WITHER) {
+						extraType = "WITHER";
+					}
+				} else if (e instanceof Creeper) {
+					Creeper cre = (Creeper) e;
+
+					if (cre.isPowered()) {
+						extraType = "POWERED";
+					}
+				} else if (e instanceof Chicken) {
+					Chicken mob = (Chicken) e;
+
+					if (mob.getPassenger() != null) {
+						extraType = "JOCKEY";
+					}
+				} else if (e instanceof Rabbit) {
+					Rabbit mob = (Rabbit) e;
+
+					if (mob.getRabbitType() == Type.THE_KILLER_BUNNY) {
+						extraType = "KILLER";
+					}
+				} else if (e instanceof Spider) {
+					Spider mob = (Spider) e;
+
+					if (mob.getPassenger() != null) {
+						extraType = "JOCKEY";
+					}
+				} else if (e instanceof Guardian) {
+					Guardian mob = (Guardian) e;
+
+					if (mob.isElder()) {
+						extraType = "ELDER";
+					}
+				}
+
+				if (extraType == null) {
+					return;
+				}
+
+				Stat mobkilled = hook.getStat(MobKilledStat.statName);
+
+				StatsHolder holder = hook.getStatsHolder(nEvent.getDamager()
+						.getUniqueId());
+
+				holder.addEntry(mobkilled, new DefaultStatEntry(1,
+						new MetadataPair("entityType", e.getType().toString()),
+						new MetadataPair("extraType", extraType),
+						new MetadataPair("world", e.getLocation().getWorld()
+								.getName().toString())));
+			}
+		}
+	}
+}
