303f556adda1b4908585ccd2a15e87267b2a6cf6
Staartvin <mijnleraar@msn.com>
2015-08-14 22:04:25 +0200

    Add UltimateCoreHook
    Signed-off-by: Staartvin <mijnleraar@msn.com>

---
+++ src/me/staartvin/plugins/pluginlibrary/hooks/UltimateCoreHook.java
@@ -0,0 +1,41 @@
+package me.staartvin.plugins.pluginlibrary.hooks;
+
+import me.staartvin.plugins.pluginlibrary.Library;
+import bammerbom.ultimatecore.bukkit.UltimateCore;
+
+/**
+ * UltimateCore library, <a
+ * href="http://dev.bukkit.org/bukkit-plugins/ultimatecore/">link</a>.
+ * <p>
+ * Date created:  17:30:19
+ * 14 aug. 2015
+ * @author Staartvin
+ *
+ */
+public class UltimateCoreHook extends LibraryHook {
+
+	private UltimateCore api;
+	
+	/* (non-Javadoc)
+	 * @see me.staartvin.plugins.pluginlibrary.hooks.LibraryHook#isAvailable()
+	 */
+	@Override
+	public boolean isAvailable() {
+		return this.getPlugin().getServer().getPluginManager()
+				.isPluginEnabled(Library.ULTIMATECORE.getPluginName());
+	}
+
+	/* (non-Javadoc)
+	 * @see me.staartvin.plugins.pluginlibrary.hooks.LibraryHook#hook()
+	 */
+	@Override
+	public boolean hook() {
+		if (!isAvailable())
+			return false;
+
+		api = (UltimateCore) this.getPlugin().getServer().getPluginManager()
+				.getPlugin(Library.ULTIMATECORE.getPluginName());
+
+		return api != null;
+	}
+}
